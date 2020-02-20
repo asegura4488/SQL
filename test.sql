2fe1eec975213cf01e2c8b66779be442295b62d4
asegura4488 <asegura4488@gmail.com>
2020-02-20 18:02:20 -0500

    New commands

--- test.sql
+++ test.sql
@@ -9,8 +9,10 @@ CREATE TABLE IF NOT EXISTS `Products` (
 INSERT INTO `Products` VALUES ('Ferreteria','Destornillador',1.34,'2020-03-01',1.0);
 INSERT INTO `Products` VALUES ('Deportes','Martillo
 ',3.75,'2019-03-04',2.0);
-INSERT INTO `Products` VALUES ('Hogar','Pantalon',1.2,'2019-03-02',3.0);
-INSERT INTO `Products` VALUES ('Hogar','Caminador
-',4.45,'2019-04-07',4.0);
+INSERT INTO `Products` VALUES ('Hogar','Computador',1.2,'2019-03-02',3.0);
+INSERT INTO `Products` VALUES ('Hogar','Computador',4.45,'2019-04-07',4.0);
 INSERT INTO `Products` VALUES ('Aseo','Escoba',1.25,'2020-02-31',5.0);
+INSERT INTO `Products` VALUES ('Hogar','Cama',0.5,'2019-10-10',6.0);
+INSERT INTO `Products` VALUES ('Hogar','Cama',0.5,'2019-10-10',6.0);
+INSERT INTO `Products` VALUES ('Hogar','Cama',0.5,'2019-10-10',6.0);
 COMMIT;
